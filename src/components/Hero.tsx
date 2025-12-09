68ca8e17f3976f557fa7ce9bef07781f52fa883e
Thushana LS <dhushanals@example.com>
2025-12-09 12:17:38 +0530

    feat(ui): add cn utility and update Hero component styles for better responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,27 +9,27 @@ export default function Hero() {
     <div className="flex w-full flex-col items-center justify-center">
       <div className="flex min-w-full flex-col items-start gap-4 rounded-3xl bg-pink-200 p-4 sm:p-20">
         {/* free shipping */}
-        <div className="flex items-center justify-center gap-2 rounded-2xl bg-pink-300 p-1">
-          <span className="rounded-2xl bg-pink-600 px-3 py-1 text-lg text-[10px] text-pink-50 md:text-xs">
+        <div className="flex items-center justify-center gap-2 rounded-4xl bg-pink-300 p-1">
+          <span className=" rounded-4xl bg-pink-600 px-3 py-2 sm:py-1 text-sm text-pink-50 ">
             NEWS
           </span>
-          <p className="text-[10px] text-pink-600 md:text-sm">
+          <p className="text-sm text-pink-600 ">
             Free Shippings Order Above LKR 10000!
           </p>
-          <ChevronRightIcon className="h-4 w-4 text-pink-600" />
+          <ChevronRightIcon className="sm:size-4 size-6 text-pink-600" />
         </div>
 
         {/* title */}
-        <h1 className="bg-linear-to-r from-slate-600 to-red-400 bg-clip-text text-6xl text-transparent  mb-10">
+        <h1 className="mb-1 bg-linear-to-r from-slate-600 to-red-400 bg-clip-text text-2xl text-transparent md:mb-10 md:text-6xl">
           Gadgets you'll love.
           <br />
           Prices you'll trust.
         </h1>
 
         {/* starting price */}
-        <div className="flex flex-col text-slate-700 gap-2">
+        <div className="flex flex-col gap-2 text-slate-700">
           <p className="text-sm leading-0">Starts From</p>
-          <span className="font-semibold text-2xl">{currency} 1200</span>
+          <span className="text-2xl font-semibold">{currency} 1200</span>
         </div>
       </div>
     </div>
